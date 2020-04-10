26a7397fb9374edae965129ae6c9733b76c27e1b
Patrick Krämer <patagonahn@gmail.com>
2020-04-10 13:41:29 +0200

    fix permissions, show upload errors

--- upload.sh
+++ upload.sh
@@ -5,13 +5,13 @@ source ./config.env
 echo starting upload
 for f in ${OUT_DIR}/meta/*; do
   echo $f
-  curl --silent --fail -T $f ${WEBDAV_URL}/meta/`basename -- $f` && rm $f
+  curl --silent --fail --show-error -T $f ${WEBDAV_URL}/meta/`basename -- $f` && rm $f
 done
 for f in ${OUT_DIR}/images/*; do
   echo $f
-  curl --silent --fail -T $f ${WEBDAV_URL}/images/`basename -- $f` && rm $f
+  curl --silent --fail --show-error -T $f ${WEBDAV_URL}/images/`basename -- $f` && rm $f
 done
 for f in ${OUT_DIR}/audio/*; do
   echo $f
-  curl --silent --fail -T $f ${WEBDAV_URL}/audio/`basename -- $f` && rm $f
+  curl --silent --fail --show-error -T $f ${WEBDAV_URL}/audio/`basename -- $f` && rm $f
 done
