4caac6fab4e4c4fc4232b982e480b833245488e6
Patrick Krämer <patagonahn@gmail.com>
2020-04-25 14:18:36 +0200

    do not upload failed projections, fix bug that projection map was way off

--- schedule_all.sh
+++ schedule_all.sh
@@ -15,7 +15,7 @@ grep "NOAA 15" ${OUT_DIR}/weather.txt -A 2 > ${OUT_DIR}/weather.tle
 grep "NOAA 18" ${OUT_DIR}/weather.txt -A 2 >> ${OUT_DIR}/weather.tle
 grep "NOAA 19" ${OUT_DIR}/weather.txt -A 2 >> ${OUT_DIR}/weather.tle
 
-# wxmap has a parameter to use specific tle files, wxproj doesn't
+# wxmap has a parameter to use specific tle files and looks in ./weather.txt first, wxproj doesn't
 cp ${OUT_DIR}/weather.txt /usr/local/lib/wx/tle/weather.txt
 
 #Remove all AT jobs
